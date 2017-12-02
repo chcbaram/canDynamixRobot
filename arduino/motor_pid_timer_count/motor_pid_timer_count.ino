76427e637db3f0c3aed15a52fb4cbd9d0c7bcbcd
HanCheol Cho <chcbaram@paran.com>
2017-12-03 01:08:01 +0900

    코드 정리

--- arduino/motor_pid_timer_count/motor_pid_timer_count.ino
+++ arduino/motor_pid_timer_count/motor_pid_timer_count.ino
@@ -2,11 +2,6 @@
 
 
 
-#define ENCODER_RES     1400.
-#define WHEEL_LENGTH    (34.*PI)
-
-int32_t go_count;
-
 void setup() 
 {
   motorBegin();
@@ -15,10 +10,6 @@ void setup()
   motorMoveSpeed(0, 0);
   delay(1000);
   motorMoveSpeed(0, 0);
-
-  go_count = (int32_t)(100. * 1400. / WHEEL_LENGTH);
-
-
 }
 
 void loop() 
@@ -28,14 +19,5 @@ void loop()
   Serial.print(" ");
   Serial.println(motorGetCounter(R_MOTOR));
   delay(20);
-  /*
-  if (motorGetCounter(L_MOTOR) >= go_count)
-  {
-    motorMoveSpeed(0, 0);
-    Serial.println(motorGetCounter(R_MOTOR));
-    delay(100);    
-  }
-  */
-
 }
 
